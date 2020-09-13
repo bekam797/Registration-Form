d61a9f7cc8e4a48d42aeead935bdcf7140f7b138
Beka <bekamakharoblishvili@gmail.com>
2020-09-13 18:52:05 +0400

    Modifie app.js component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+
+import { history } from './helper/history';
+import RegisterContainer from './components/RegisterContainer/Registercontainer';
+import SuccessPage from './components/SuccessPage/SuccessPage';
+import Layout from './Layout/Layout';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <div>hello</div>
+      <div className="col-sm-8 col-sm-offset-2">
+        <Router history={history}>
+          <Switch>
+            <Layout>
+              <Route exact path="/success" component={SuccessPage} />
+              <Route exact path="/" component={RegisterContainer} />
+            </Layout>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
