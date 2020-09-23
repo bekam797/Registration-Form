d5e96aa8fb69a3dd5b7157313bd5037519137d6d
Beka <bekamakharoblishvili@gmail.com>
2020-09-23 19:32:35 +0400

    Made validation func for every field

---
+++ src/validation/validation.js
@@ -0,0 +1,61 @@
+export const validate = (name, value) => {
+  const notEmpty = value.length > 0;
+  const validEmailRegex = RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+  let errorFlag = '';
+
+  switch (name) {
+    case 'firstName':
+      errorFlag =
+        notEmpty && value.length <= 2
+          ? 'სახელი უნდა შეიცავდეს მინიმუმ 2 სიმბოლოს'
+          : '';
+      break;
+    case 'lastName':
+      errorFlag =
+        notEmpty && value.length <= 4
+          ? 'სახელი უნდა შეიცავდეს მინიმუმ 4 სიმბოლოს'
+          : '';
+      break;
+    case 'personalNumber':
+      errorFlag =
+        (notEmpty && value.length < 8) || value.length > 15
+          ? 'პირადი ნომერი უნდა შეიცავდეს 8 დან 15 სიმბოლომდე'
+          : '';
+      break;
+    case 'email':
+      errorFlag =
+        notEmpty && !validEmailRegex.test(value)
+          ? 'გთხოვთ, შეიყვანოთ ელექტრონული ფოსტის მისამართი'
+          : '';
+      break;
+    case 'userName':
+      errorFlag =
+        notEmpty && value.length <= 4
+          ? 'მომხმარებლის სახელი უნდა შეიცავდეს მინიმუმ 4 სიმბოლოს'
+          : '';
+      break;
+    case 'password':
+      errorFlag =
+        notEmpty && value.length <= 6
+          ? 'პაროლი უნდა შედგებოდეს მინიმუმ 6 სიმბოლოსგან'
+          : '';
+      break;
+    default:
+      break;
+  }
+  return errorFlag;
+};
+
+export const validateForm = (data) => {
+  let valid = true;
+
+  Object.values(data).forEach((val) => {
+    if (val.value.length > 0 && val.error.length > 0) {
+      valid = true;
+    } else if (val.value === '' || val.error.length > 0) {
+      valid = false;
+    }
+  });
+
+  return valid;
+};
